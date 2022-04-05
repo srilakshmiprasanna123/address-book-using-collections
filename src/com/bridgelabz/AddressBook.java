d57c79dc3a24e169aef315d3e9a630943e246b1a
srilakshmiprasanna123 <prasanna98ece.rymec@gmail.com>
2022-04-05 15:00:57 +0530

    remove the duplicate contacts

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -3,11 +3,13 @@ package com.bridgelabz;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class AddressBook {
     static Scanner sc = new Scanner(System.in);
-    static  ArrayList<Person> adressBook = new ArrayList<Person>();
+    static List<Person> adressBook = new ArrayList<Person>();
     static Person addressBoook=new Person();
 
     public static void main(String[] args) {
@@ -21,8 +23,8 @@ public class AddressBook {
             switch(options) {
                 case 1:
                     boolean condition1 = true;
-                    while ((condition1)== true) {
-                        System.out.println("1.add" +"\n" +"2.display"+ "\n" + "3.edit"+"\n" +"4.delete"+"\n" +"5.exit");
+                    while (condition1== true) {
+                        System.out.println("1.add" +"\n" +"2.display"+ "\n" + "3.edit"+"\n" +"4.delete"+"\n"+"5.RemoveDuplicates"+"\n"+"6.exit");
                         int option = sc.nextInt();
                         switch (option) {
                             case 1:
@@ -38,6 +40,10 @@ public class AddressBook {
                                 addressBookList.deleteContact();
                                 break;
                             case 5:
+                                System.out.println("enter a  name");
+                                String personName=sc.next();
+                                addressBookList.removeDuplicates(personName);
+                            case 6:
                                 condition1 = false;
                                 break;
                             default:
@@ -46,14 +52,14 @@ public class AddressBook {
                     }
                     break;
                 case 2:
-                    HashMap<String, ArrayList<Person>> map = new HashMap<>();
-                    ArrayList<Person> arrayList = new ArrayList<>();
+                    HashMap<String, List<Person>> map = new HashMap<>();
+                    List<Person> arrayList = new ArrayList<>();
                     Person addessBook1=addressBoook;
                     arrayList.add(addessBook1);
                     map.put("Ram", arrayList);
                     System.out.println(map);
                     if (map.containsKey("Ram")) {
-                        ArrayList<Person> ram = map.get("Ram");
+                        List<Person> ram = map.get("Ram");
                         Person addessBook2=addressBoook;
                         ram.add(addessBook2);
                         map.put("Ram", ram);
@@ -70,36 +76,31 @@ public class AddressBook {
         }
     }
 
+    public void removeDuplicates(String personName) {
+        List<Person> AfterRemoveDuplicates=adressBook.stream().filter(person->person.getFirstName().equalsIgnoreCase(personName)).distinct().collect(Collectors.toList());
+        System.out.println("After a removing duplicate elements"+AfterRemoveDuplicates);
+    }
+
     public void addContactDetails() {
         Person details=new Person();
         System.out.println("Enter a first name:");
-        String firstName=sc.next();
         details.setFirstName(sc.next());
         System.out.println("Enter a last name:");
-        String lastName=sc.next();
         details.setLastName(sc.next());
         System.out.println("Enter a Address:");
-        String address=sc.next();
         details.setAddress(sc.next());
         System.out.println("Enter a City name:");
-        String city=sc.next();
         details.setCity(sc.next());
         System.out.println("Enter a state:");
-        String state=sc.next();
         details.setState(sc.next());
         System.out.println("Enter a email:");
-        String email=sc.next();
         details.setEmail(sc.next());
         System.out.println("Enter a zip code:");
-        int  zip=sc.nextInt();
         details.setZip(sc.nextInt());
         System.out.println("Enter a phone number:");
-        long phoneNumber=sc.nextLong();
         details.setPhoneNumber(sc.nextLong());
 
         adressBook.add(details);
-        Person details1=new Person(firstName, lastName, address,  city, state, email,  zip, phoneNumber);
-        adressBook.add(details1);
         System.out.print(adressBook);
         System.out.println("succesfully added person new contacts");
     }
